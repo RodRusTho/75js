868d7b6dd738681049d0f8664df5e860d6f68c1f
Rodney Thomas <114374607+RodRusTho@users.noreply.github.com>
2022-09-26 14:01:35 -0500

    Update engine.js

--- src/engine.js
+++ src/engine.js
@@ -40,3 +40,32 @@ function config(w1,h1,c1) {
   document.getElementById("Main-Screen").height = height1;
   document.getElementById("Main-Screen").style.backgroundColor = colorToSet;
 }
+
+// This file is for audio sources and audio playing.
+
+// Function to play a sound
+function playSound(s1) {
+  var path = s1;
+  let sound = new Audio(path);
+  sound.load();
+  sound.play();
+  sound.load();
+}
+
+// Function to play background music
+function bgMPlay(a1,v1) {
+  var audId = a1;
+  var volNu = v1;
+  window.addEventListener("DOMContentLoaded", event => {
+    const audio = document.querySelector(audId);
+    audio.volume = v1;
+    audio.play();
+  });
+}
+
+// Function to pause background music
+function bgMPause(a1) {
+  var audId = a1;
+  const audio = document.querySelector(audId);
+  audio.pause();
+}
