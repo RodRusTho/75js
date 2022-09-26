ba22a419688f8a1c8eebafb69bdbb47e7d31dcd4
Rodney Thomas <114374607+RodRusTho@users.noreply.github.com>
2022-09-26 12:25:29 -0500

    Update engine.js

--- src/engine.js
+++ src/engine.js
@@ -1,6 +1,9 @@
 // This file includes all the basic functions to get started using the engine
 // Read the LICENSE file in the main directory of the engine.
 
+// Defines the canvas and context = 2d
+var c = document.getElementById("Main-Screen");
+var ctx = c.getContext("2d");
 
 // Function to define a screen height and width.
 function cScreenSize(w1,h1) {
@@ -12,8 +15,6 @@ function cScreenSize(w1,h1) {
 
 // Function to create entities such as players, floors, enemies and such.
 function createEnt(imgSrc,p1,p2,w1,w2) {
-  var c = document.getElementById("Main-Screen");
-  var ctx = c.getContext("2d");
   var img = document.getElementById(imgSrc);
   var pos1 = p1;
   var pos2 = p2;
