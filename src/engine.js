4f82d9fc2db58df67868ada0a474543ca4076fb1
Rodney Thomas <114374607+RodRusTho@users.noreply.github.com>
2022-09-26 09:43:28 -0500

    Update engine.js

--- src/engine.js
+++ src/engine.js
@@ -1,8 +1,10 @@
-function createEnt(imgSrc,p1,p2) {
+function createEnt(imgSrc,p1,p2,w1,w2) {
   var c = document.getElementById("Main-Screen");
   var ctx = c.getContext("2d");
   var img = document.getElementById(imgSrc);
   var pos1 = p1
   var pos2 = p2
-  ctx.drawImage(img,pos1,pos2);
+  var wid1 = w1
+  var wid2 = w2
+  ctx.drawImage(img,pos1,pos2,wid1,wid2);
 };
