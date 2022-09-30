ea2c99387b4b147d700accfb311dc8f9e1db0081
Rodney Thomas <114374607+RodRusTho@users.noreply.github.com>
2022-09-30 14:57:58 -0500

    Create core.js

---
+++ src/core.js
@@ -0,0 +1,29 @@
+class Seventy{
+  function init() {
+    
+  }
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  // Start the game and run code.
+  function initWindow(caW, caH, caName) {
+    // Define canvas width, height and ID from
+    // what the user input into the attributes.
+    const canvasWidth  = caW;
+    const canvasHeight = caH;
+    const c            = caName;
+    
+    // Change the canvas height and width of canvas
+    document.getElementById(canvasId).style.width  = caW;
+    document.getElementById(canvasId).style.height = caH;
+    
+    return canvasWidth, canvasHeight, c;
+  }
+}
